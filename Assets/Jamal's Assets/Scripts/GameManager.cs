2de6c3e626b5a823aa4f00b4428495126fb60ce6
Jamal Woods <JamalCWoods@Gmail.com>
2021-01-30 14:43:18 -0500

    added a end of game screen

--- Assets/Jamal's Assets/Scripts/GameManager.cs
+++ Assets/Jamal's Assets/Scripts/GameManager.cs
@@ -13,6 +13,9 @@ public class GameManager : MonoBehaviour
     private GameObject oxygenBar,fuelBar;
 
     [SerializeField]
+    private GameObject endGameScreen;
+
+    [SerializeField]
     private GameObject stats;
 
     private int spawnRange = 50;
@@ -24,29 +27,41 @@ public class GameManager : MonoBehaviour
     private int minDebriSize = 1;
     private int maxDebriSize = 4;
 
-    private GameObject playerInstance, keyInstance;
+    private GameObject playerInstance, keyInstance, shipInstance;
 
     private List<GameObject> itemInstances = new List<GameObject>(), debriInstances = new List<GameObject>(),entityInstances = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < oxygenSpawnAmount; i++) {
-            Vector3 v = new Vector3(Random.Range(-spawnRange, spawnRange), Random.Range(-spawnRange, spawnRange),0);
+        startGame();
+    }
+
+    private void SpawnCollectables()
+    {
+        for (int i = 0; i < collectableSpawnAmount; i++)
+        {
+            Vector3 v = new Vector3(Random.Range(-spawnRange, spawnRange), Random.Range(-spawnRange, spawnRange), 0);
             while (!checkForOpenSpawn(v))
             {
                 v = new Vector3(Random.Range(-spawnRange, spawnRange), Random.Range(-spawnRange, spawnRange), 0);
             }
-            GameObject g = Instantiate(ItemPreab, v,Quaternion.identity);
-            ItemManager item = g.GetComponent<ItemManager>();
+            GameObject g = Instantiate(ItemPreab, v, Quaternion.identity);
             g.transform.parent = transform;
-            item.Type = ItemTypes.Oxygen;
-            item.OxygenReplinishAmount = 20;
-            item.GetComponent<SpriteRenderer>().color = Color.blue;
+            ItemManager item = g.GetComponent<ItemManager>();
+            item.Type = ItemTypes.Collectable;
+            int cIndex = Random.Range(0, 3);
+            item.CType = (CollectableTypes)cIndex;
+            // magenta - rebreather, white - armor, gray - propulsion computer
+            item.GetComponent<SpriteRenderer>().color = new Color[] { Color.magenta, Color.white, Color.gray }[cIndex];
             itemInstances.Add(g);
             entityInstances.Add(g);
         }
+    }
 
-        for (int i = 0; i < propellantSpawnAmount; i++) {
+    private void SpawnPropellants()
+    {
+        for (int i = 0; i < propellantSpawnAmount; i++)
+        {
             Vector3 v = new Vector3(Random.Range(-spawnRange, spawnRange), Random.Range(-spawnRange, spawnRange), 0);
             while (!checkForOpenSpawn(v))
             {
@@ -63,26 +78,16 @@ public class GameManager : MonoBehaviour
             itemInstances.Add(g);
             entityInstances.Add(g);
         }
+    }
 
-        for (int i = 0; i < collectableSpawnAmount; i++)
-        {
-            Vector3 v = new Vector3(Random.Range(-spawnRange, spawnRange), Random.Range(-spawnRange, spawnRange), 0);
-            while (!checkForOpenSpawn(v))
-            {
-                v = new Vector3(Random.Range(-spawnRange, spawnRange), Random.Range(-spawnRange, spawnRange), 0);
-            }
-            GameObject g = Instantiate(ItemPreab, v, Quaternion.identity);
-            g.transform.parent = transform;
-            ItemManager item = g.GetComponent<ItemManager>();
-            item.Type = ItemTypes.Collectable;
-            int cIndex = Random.Range(0, 3);
-            item.CType = (CollectableTypes)cIndex;
-            // magenta - rebreather, white - armor, gray - propulsion computer
-            item.GetComponent<SpriteRenderer>().color = new Color[] { Color.magenta, Color.white, Color.gray}[cIndex];
-            itemInstances.Add(g);
-            entityInstances.Add(g);
-        }
+    private void SpawnPlayer()
+    {
+        playerInstance = Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+        entityInstances.Add(playerInstance);
+    }
 
+    private void SpawnDebri()
+    {
         for (int i = 0; i < debriSpawnAmount; i++)
         {
             Vector3 v = new Vector3(Random.Range(-spawnRange, spawnRange), Random.Range(-spawnRange, spawnRange), 0);
@@ -93,14 +98,15 @@ public class GameManager : MonoBehaviour
             GameObject g = Instantiate(debriPrefab, v, Quaternion.identity);
             g.transform.parent = transform;
             int size = Random.Range(minDebriSize, maxDebriSize);
-            g.transform.localScale = new Vector3(size,size,1);
+            g.transform.localScale = new Vector3(size, size, 1);
             g.GetComponent<Rigidbody2D>().mass *= size;
             debriInstances.Add(g);
             entityInstances.Add(g);
         }
+    }
 
-
-        playerInstance = Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+    private void SpawnKeys()
+    {
         Vector3 V = new Vector3(Random.Range(-spawnRange, spawnRange), Random.Range(-spawnRange, spawnRange), 0);
         while (!checkForOpenSpawn(V))
         {
@@ -112,7 +118,47 @@ public class GameManager : MonoBehaviour
         Item.CType = CollectableTypes.Keys;
         Item.GetComponent<SpriteRenderer>().color = Color.yellow;
         Item.transform.localScale = new Vector3(3, 3, 1);
+        entityInstances.Add(keyInstance);
+    }
 
+    private void SpawnOxygen()
+    {
+        for (int i = 0; i < oxygenSpawnAmount; i++)
+        {
+            Vector3 v = new Vector3(Random.Range(-spawnRange, spawnRange), Random.Range(-spawnRange, spawnRange), 0);
+            while (!checkForOpenSpawn(v))
+            {
+                v = new Vector3(Random.Range(-spawnRange, spawnRange), Random.Range(-spawnRange, spawnRange), 0);
+            }
+            GameObject g = Instantiate(ItemPreab, v, Quaternion.identity);
+            ItemManager item = g.GetComponent<ItemManager>();
+            g.transform.parent = transform;
+            item.Type = ItemTypes.Oxygen;
+            item.OxygenReplinishAmount = 20;
+            item.GetComponent<SpriteRenderer>().color = Color.blue;
+            itemInstances.Add(g);
+            entityInstances.Add(g);
+        }
+    }
+
+    public void restartGame()
+    {
+        foreach(GameObject g in entityInstances)
+        {
+            GameObject.Destroy(g);
+        }
+        endGameScreen.SetActive(false);
+        startGame();
+    }
+
+    private void startGame()
+    {
+        SpawnOxygen();
+        SpawnCollectables();
+        SpawnPropellants();
+        SpawnDebri();
+        SpawnPlayer();
+        SpawnKeys();
         SpawnShip();
     }
 
@@ -124,21 +170,22 @@ public class GameManager : MonoBehaviour
         switch (shipEdge)
         {
             case 0:
-                Instantiate(shipPrefab, new Vector2(Random.Range(-55, 55), 60), Quaternion.identity);
+                shipInstance = Instantiate(shipPrefab, new Vector2(Random.Range(-55, 55), 60), Quaternion.identity);
                 break;
 
             case 1:
-                Instantiate(shipPrefab, new Vector2(60, Random.Range(-55, 55)), Quaternion.identity);
+                shipInstance = Instantiate(shipPrefab, new Vector2(60, Random.Range(-55, 55)), Quaternion.identity);
                 break;
 
             case 2:
-                Instantiate(shipPrefab, new Vector2(Random.Range(-55, 55), -60), Quaternion.identity);
+                shipInstance = Instantiate(shipPrefab, new Vector2(Random.Range(-55, 55), -60), Quaternion.identity);
                 break;
 
             case 3:
-                Instantiate(shipPrefab, new Vector2(-60, Random.Range(-55, 55)), Quaternion.identity);
+                shipInstance = Instantiate(shipPrefab, new Vector2(-60, Random.Range(-55, 55)), Quaternion.identity);
                 break;
         }
+        entityInstances.Add(shipInstance);
     }
 
     bool checkForOpenSpawn(Vector3 v)
@@ -174,6 +221,12 @@ public class GameManager : MonoBehaviour
         {
             RectTransform r = fuelBar.GetComponent<RectTransform>();
             r.sizeDelta = new Vector2(0, r.sizeDelta.y);
+        }
+
+        if (playerInstance.GetComponent<PlayerManager>().oxygenAmount < 0)
+        {
+            playerInstance.SetActive(false);
+            endGameScreen.SetActive(true);
         }
     }
 }
