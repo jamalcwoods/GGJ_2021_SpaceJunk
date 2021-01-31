424cea7f4ca78fee1115b83cbc65a4000931f0bf
Jamal Woods <JamalCWoods@Gmail.com>
2021-01-31 15:30:48 -0500

    removed win key

--- Assets/Jamal's Assets/Scripts/GameManager.cs
+++ Assets/Jamal's Assets/Scripts/GameManager.cs
@@ -283,10 +283,6 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            WinGame();
-        }
 
         if(Input.GetKeyDown(KeyCode.Escape) && !isPaused)
         {
